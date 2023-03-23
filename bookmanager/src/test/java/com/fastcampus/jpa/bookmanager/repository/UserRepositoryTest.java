c6fd4feb2b67e1cf021f7f6a6782ba0105927e57
MinyongPark <86906386+Minon98@users.noreply.github.com>
2023-03-23 22:14:40 +0900

    Update query method
    query method practice

---
+++ bookmanager/src/test/java/com/fastcampus/jpa/bookmanager/repository/UserRepositoryTest.java
@@ -0,0 +1,41 @@
+package com.fastcampus.jpa.bookmanager.repository;
+
+import com.fastcampus.jpa.bookmanager.domain.User;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserRepositoryTest {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    void crud() { // create, read, update, delete
+        userRepository.save(new User("david", "david@fastcampus.com"));
+
+        User user = userRepository.findById(1L).orElseThrow(RuntimeException::new);
+        user.setEmail("martin-updated@fastcampus.com");
+
+        userRepository.save(user);
+    }
+
+    @Test
+    void select() {
+        System.out.println(userRepository.findByName("dennis"));
+
+        System.out.println("findByEmail : " + userRepository.findByEmail("martin@fastcampus.com"));
+        System.out.println("getByEmail : " + userRepository.getByEmail("martin@fastcampus.com"));
+        System.out.println("readByEmail : " + userRepository.readByEmail("martin@fastcampus.com"));
+        System.out.println("queryByEmail : " + userRepository.queryByEmail("martin@fastcampus.com"));
+        System.out.println("searchByEmail : " + userRepository.searchByEmail("martin@fastcampus.com"));
+        System.out.println("streamByEmail : " + userRepository.streamByEmail("martin@fastcampus.com"));
+        System.out.println("findUserByEmail : " + userRepository.findUserByEmail("martin@fastcampus.com"));
+
+        System.out.println("findSomethingByEmail : " + userRepository.findSomethingByEmail("martin@fastcampus.com"));
+
+        System.out.println("findTop2ByName : " + userRepository.findTop2ByName("martin"));
+        System.out.println("findFirst2ByName : " + userRepository.findFirst2ByName("martin"));
+        System.out.println("findLast1ByName : " + userRepository.findLast1ByName("martin"));
+    }
+}
