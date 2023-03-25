4d2cadc08afd7ff755c3c76c3b2b556d83f187c4
MinyongPark <86906386+Minon98@users.noreply.github.com>
2023-03-25 22:35:28 +0900

    Update entity listener

--- bookmanager/src/main/java/com/fastcampus/jpa/bookmanager/domain/User.java
+++ bookmanager/src/main/java/com/fastcampus/jpa/bookmanager/domain/User.java
@@ -1,15 +1,8 @@
 package com.fastcampus.jpa.bookmanager.domain;
 
 import java.time.LocalDateTime;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Index;
-import javax.persistence.Table;
-import javax.persistence.UniqueConstraint;
+import javax.persistence.*;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -24,8 +17,9 @@ import lombok.RequiredArgsConstructor;
 @Data
 @Builder
 @Entity
-@Table(name = "user", indexes = { @Index(columnList = "name")}, uniqueConstraints = {@UniqueConstraint(columnNames = {"email"})})
-public class User {
+@EntityListeners(value = MyEntityListener.class)
+//@Table(name = "user", indexes = { @Index(columnList = "name")}, uniqueConstraints = {@UniqueConstraint(columnNames = {"email"})})
+public class User implements Auditable{
     @Id
     @GeneratedValue
     private Long id;
@@ -49,4 +43,34 @@ public class User {
 
 //    @OneToMany(fetch = FetchType.EAGER)
 //    private List<Address> address;
+
+//    @PrePersist
+//    public void prePersist(){
+//        this.createdAt=LocalDateTime.now();
+//        this.updatedAt=LocalDateTime.now();
+//    }
+//    @PostUpdate
+//    public void postUpdate() {
+//        this.updatedAt=LocalDateTime.now();
+//    }
+//    @PreUpdate
+//    public void preUpdate(){
+//        System.out.println(">>> preUpdate");
+//    }
+//    @PreRemove
+//    public void preRemove(){
+//        System.out.println(">>> preRemove");
+//    }
+//    @PostPersist
+//    public void postPersist(){
+//        System.out.println(">>> postPersist");
+//    }
+//    @PostRemove
+//    public void postRemove(){
+//        System.out.println(">>> postRemove");
+//    }
+//    @PostLoad
+//    public void postLoad(){
+//        System.out.println(">>> postLoad");
+//    }
 }
---
+++ bookmanager/src/test/java/com/fastcampus/jpa/bookmanager/repository/BookRepositoryTest.java
@@ -0,0 +1,26 @@
+package com.fastcampus.jpa.bookmanager.repository;
+
+import com.fastcampus.jpa.bookmanager.domain.Book;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class BookRepositoryTest {
+    @Autowired
+    private BookRepository bookRepository;
+
+    @Test
+    void bookTest(){
+        Book book = new Book();
+        book.setName("Jpa 초격자 패키지");
+        book.setAuthor("fastcampus");
+
+        bookRepository.save(book);
+
+        System.out.println(bookRepository.findAll());
+    }
+
+}
